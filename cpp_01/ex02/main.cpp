37746266b5f6e6c3aba33d559985511ddfd7dafa
Paco Rocha <frocha@f-rocha.com>
2023-01-06 17:38:13 +0100

    corrected erro in ex02

--- cpp_01/ex02/main.cpp
+++ cpp_01/ex02/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jfrancis <jfrancis@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/28 17:58:28 by jfrancis          #+#    #+#             */
-/*   Updated: 2022/12/28 19:57:00 by jfrancis         ###   ########.fr       */
+/*   Updated: 2023/01/06 17:36:42 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	main(void) {
 	std::string& stringREF = string;
 
 	std::cout << "Memory address of string is: " << &string << std::endl;
-	std::cout << "Memory address of stringPTR is: " << &stringPTR << std::endl;
+	std::cout << "Memory address of stringPTR is: " << stringPTR << std::endl;
 	std::cout << "Memory address of stringREF is: " << &stringREF << std::endl;
 	std::cout << std::endl;
 	std::cout << "The value of string is: " << string << std::endl;
