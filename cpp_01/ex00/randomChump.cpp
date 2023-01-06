effc6f86555b724155546e2a3df89e7339322e61
Paco Rocha <frocha@f-rocha.com>
2023-01-06 03:35:36 +0100

    final version of CPP 01 (I hope)

--- cpp_01/ex00/randomChump.cpp
+++ cpp_01/ex00/randomChump.cpp
@@ -3,22 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   randomChump.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
+/*   By: jfrancis <jfrancis@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/06 03:08:40 by coder             #+#    #+#             */
-/*   Updated: 2023/01/06 03:14:56 by coder            ###   ########.fr       */
+/*   Updated: 2023/01/05 23:29:27 by jfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
 void Zombie::randomChump(std::string name) {
-	Zombie	chump(name);
-
-	chump.announce();
-}
-
-void randomChump(std::string name) {
 	Zombie	chump(name);
 
 	chump.announce();
